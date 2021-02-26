b4f4a8c718379ba978fa9c5e2790e1775f37ab6e
Adam Lukowski <adamsowalukowski@gmail.com>
2021-02-25 19:10:36 -0600

    Generates GamePlatform model, migration files, and alters game.rb and platform.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
   has_many :game_developers
   has_many :developers, through: :game_developers
+  has_many :game_platforms
+  has_many :platforms, through: :game_platforms
 end
