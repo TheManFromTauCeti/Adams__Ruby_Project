e215938cfb0736856a9dd2f7614a96006cb9e705
Adam Lukowski <adamsowalukowski@gmail.com>
2021-02-25 18:59:52 -0600

    Generates model Developer and migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_005539) do
+ActiveRecord::Schema.define(version: 2021_02_26_005844) do
+
+  create_table "developers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "country"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
